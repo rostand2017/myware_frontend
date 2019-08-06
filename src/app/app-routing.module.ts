f4582ffa89e0a848045d0bb7fd0cff422fd789f7
Ross <rostandfotso32@gmail.com>
2019-08-06 06:01:04 +0100

    template authentification présent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthComponent} from './auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'login', component: AuthComponent},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,52 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatAccordion,
+    MatBadgeModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule, MatSnackBarModule, MatTableModule, MatToolbarModule,
+} from '@angular/material';
+import { MenuComponent } from './menu/menu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {UserService} from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MenuComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatMenuModule,
   ],
-  providers: [],
+  providers: [
+      UserService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
